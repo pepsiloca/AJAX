7a129641478d52b828037ed4e2e9d8e2471711bf
u8931011 <u8931011@gmail.com>
2020-06-18 13:25:54 +0800

    完成編輯功能,並增加取消表單操作的功能/ajax 查詢提示功能

--- app2/api/insert.php
+++ app2/api/insert.php
@@ -2,6 +2,8 @@
 //處理新增資料的請求
 include_once "base.php";
 
+
+//前端採用POST的方式送出請求，因此API這邊使用$_POST來接收請求
 if(!empty($_POST)){
     $row=[];
     $row['name']=$_POST['name'];
@@ -15,12 +17,11 @@ if(!empty($_POST)){
     $row['dept']=$_POST['dept'];
     $row['grad_at']=$_POST['grad_at'];
 
+    $db=new DB("students");
+    
+    //在save()函式中我們使用exec($sql)來送出sql語句，此函式會回應受影響的筆數
+    //因此如果新增成功會回應1，新增失敗會回應0，以此回應來提供前端做為判斷
+    echo $db->save($row);
 
-$db=new DB("student");
-$db->save($row);
-echo 1;
-
-}else{
-    echo 0;
 }
 ?>
