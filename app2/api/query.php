949846bbc82cfce70068d98f8a2b3f718e3b78ff
u8931011 <u8931011@gmail.com>
2020-06-18 15:17:19 +0800

    上傳

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,4 +1,3 @@
-  
 <?php
 //處理查詢資料的請求
 include_once "base.php";
@@ -11,8 +10,20 @@ $db=new DB("students");
 $query=$db->all([]," where name like '%$str%'");
 
 //利用迴圈來列出查詢到的資料並加上需要的html標籤或其它內容
-foreach($query as $q){
-    echo "<div>結果一:".$q['name']."-".$q['class_num']."-".$q['dept']."</div>";
-}
+$data=[];
 
+    foreach($query as $q){
+        $data[]=[
+           '_id'=>$q['id'],
+            '學號'=>$q['uni_id'],
+            '班級座號'=>mb_substr($q['class_num'],3,2),
+            '班級'=>mb_substr($q['class_num'],0,3),
+            '姓名'=>$q['name'],
+            '畢業國中'=>mb_substr($q['grad_at'],0,-2,'utf8'),
+            '科別'=>$q['dept']
+        ];
+    }
+
+echo json_encode($data);
+//echo json_encode($query);
 ?>
